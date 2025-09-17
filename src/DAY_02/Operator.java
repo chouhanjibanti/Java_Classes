add8197cc2293214d564224e97c89684ca17775b
Banti Chouhan <chouhanbanti224@gmail.com>
2025-09-17 20:04:20 +0530

    day2

---
+++ src/DAY_02/Operator.java
@@ -0,0 +1,105 @@
+package DAY_02;
+
+public class Operator {
+
+	public static void main(String[] args) {
+//		  int a = 20;
+//		  int b = 10;
+//		  System.out.println(a+b);
+//		  System.out.println(a-b);
+//		  System.out.println(a*b);
+//		  System.out.println(a/b);
+//		  System.out.println(a%b);
+//		  
+//		  // concatination operator 
+//		 int a1 = 10;
+//		 String s1 = "a";
+//		 System.out.println(a1+s1);// 10a
+		
+		
+		// Comparison (== , < ,> , <= , >= , !=)
+//		System.out.println(5==5);//T
+//		System.out.println(5>2); // T
+//		System.out.println(5<2); //F
+//		System.out.println(3>=3); //T
+//		System.out.println(2<=3.0); //T
+		
+		
+		// Assignment Operator (= , += , -= , *= , /= , %=)
+//		int a = 5;
+//		System.out.println(a = a+5);
+//		System.out.println(a+=5);
+		
+		// increment and decrement 
+		// post and pre
+		
+//		int a = 5;
+//		int a1=a++;
+////		++a;
+//		System.out.println(a1);
+//		System.out.println(a);
+		
+		
+//		int a = 10;
+////		        10     12   12    13  +5 = 26
+//		int b = a++ + ++a + a++ - a++ +5;
+////	            14    27     27   -> 14
+//		int c = a++ + ++b - b++;
+//		System.out.println(a++ + b ); // 15+28 - 43
+//		System.out.println(b+c); // 28+14 = 42
+		
+// decrement  post and pre 
+//		int num = 50;
+//		num--;
+//		--num;
+//		System.out.println(num);//48
+		
+		
+		
+		//logical operator AND[&&] and OR(||) and NOT(!)
+		
+//		AND :- 
+//		OR :-
+//		NOT :- 
+		
+		
+		int a = 10;
+		int b = 20;
+		int c = 10;
+		System.out.println((a==c)&&(a==b));
+		System.out.println((a==c)||(a==b));
+		System.out.println(a!=10);
+		
+		
+		
+		
+	
+
+	}
+}
+
+// Operator :-    10+10
+// Arithmetic 
+// Comparison or Relational Operator 
+// Assignment Operator or Initialization operator 
+// Increment and Decrement 
+// Logical Operator 
+// Bitwise Operator
+
+
+//char  :- 'a' , 'v'
+// String :- "silicon" , "mhow"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/DataType/Variables.java
+++ src/DataType/Variables.java
@@ -1,5 +1,55 @@
 package DataType;
 
 public class Variables {
+	
+	String instituteName = "debugshala";// NPD and global scope
 
+	public static void main(String[] args) {
+		
+		int i = 10; // PDT & Local scope
+		i = 20;
+				
+//		int a ;// declaration
+//		int a = 10;// initialization
+		
+		
+	}
+	
+	double d = 10.5;// PDT and Global Scope 
 }
+
+// Variable :- variable are use to store the values and we need to follow two instructions
+//1. variable declaration
+//2. variable initialization
+
+
+// classification of variables :- 
+
+// we have two set of classification of variables.
+//1. Based on the DT
+
+//   -> Primitive Variable
+//   -> Non Primitive Variable
+
+
+//2. Based on scope 
+//    -> Local Variable
+//    -> Global Variable
+
+
+// local variable :- Any variable declared within the block(method block , conditional block , looping block , try-catch , finally etc
+// Global Variable :- Any variable declared outside the method block but within the class are known as global variable.
+
+
+//================================================================
+
+
+// we can not create  two local variabale with the same name .
+// we can reinitilize the local variable but value will get new value.
+
+
+
+
+
+
+
